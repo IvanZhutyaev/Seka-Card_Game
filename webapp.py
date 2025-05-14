958e8305a3cdd72a1f369a6195d86d0beecd0412
ModderCodder <1modder.path1@gmail.com>
2025-05-14 23:57:59 +0300

    fix FastAPI

--- webapp.py
+++ webapp.py
@@ -12,9 +12,9 @@ async def read_root():
 
 @app.get("/Game-start.html")
 async def read_game_file():
-    if not os.path.exists("Game-start.html"):
+    if not os.path.exists("pages/Game-start.html"):
         raise HTTPException(status_code=404, detail="File not found")
-    return FileResponse("Game-start.html")
+    return FileResponse("pages/Game-start.html")
 
 # Запрещаем все остальные пути
 @app.get("/{path:path}")
@@ -22,4 +22,4 @@ async def deny_all(path: str):
     raise HTTPException(status_code=403, detail="Forbidden")
 
 # Для статических файлов (если нужно)
-# app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/pages", StaticFiles(directory="pages"), name="pages")
